4def65d3b5a4c045ef3b0fbc7d75716c02319eaf
Timo Rantalainen <tjrantal@mgail.com>
2011-10-08 13:40:39 +1100

    Added compile .sh for linux

--- compileIndeksi2011.sh
+++ compileIndeksi2011.sh
@@ -1,7 +1,7 @@
 javac -cp ".:" ui/Indeksi2011.java
-::javac Analyze/Analyze.java
-::javac ui/AnalysisThread.java
-::javac Filter/ButterworthCoefficients.java
+#javac Analyze/Analyze.java
+#javac ui/AnalysisThread.java
+#javac Filter/ButterworthCoefficients.java
 jar cfm Indeksi2011.jar manifest.mf ui ReadWDQ Filter Analyze compileIndeksi2011.bat 
 #signIndeksi.bat
 java -Xmx950m -jar Indeksi2011.jar
