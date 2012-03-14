4771b58a0afdd687f3ad54bc47a1ac623945de85
Timo Rantalainen <tjrantal@gmail.com>
2012-03-15 10:27:16 +1100

    Removed build bats.

--- compileIndeksi2011.bat
+++
@@ -1,8 +0,0 @@
-javac -cp "src" ui/Indeksi2011.java -d build/java
-javac Analyze/Analyze.java
-::javac ui/AnalysisThread.java
-::javac Filter/ButterworthCoefficients.java
-jar cfm indeksi2011.jar manifest/manifest.mf build/java/ui build/java/ReadWDQ build/java/Filter build/java/Analyze build/java/fft compileIndeksi2011.bat 
-::signIndeksi.bat
-java -Xmx950m -jar indeksi2011.jar
-
--- compileIndeksi2011.sh
+++
@@ -1,7 +0,0 @@
-javac -cp ".:" ui/Indeksi2011.java
-#javac Analyze/Analyze.java
-#javac ui/AnalysisThread.java
-#javac Filter/ButterworthCoefficients.java
-jar cfm indeksi2011.jar manifest.mf ui ReadWDQ Filter Analyze compileIndeksi2011.bat 
-#signIndeksi.bat
-java -Xmx950m -jar indeksi2011.jar
